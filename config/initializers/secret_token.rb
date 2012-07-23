da810ae3bd4bb6ed21a659b0e1490c48ac0ffc9a
Ope <ope.bukola@gmail.com>
2012-07-23 18:01:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursesky::Application.config.secret_token = '9718b867b9baf0fdc6fe18223963be4e0a687c4e5de687dd3e1e99faf3dccea5e8405a8488f9ecf7a9f0eae233985649957fab3e59a8cc7a15078b0f73cc3cdc'
