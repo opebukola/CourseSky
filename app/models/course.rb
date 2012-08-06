5a556a410375e83486fa611aabb604ad9ab41a4c
Ope <ope.bukola@gmail.com>
2012-08-06 14:35:54 -0400

    Grade Level Data

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
   attr_accessible :cover_image, :description, :published, :title,
-                  :subject_id, :category_ids, :subject, :grade_level_id
+                  :subject_id, :category_ids, :subject, :grade_level_id, :grade_level
 
   has_many :lessons, dependent: :destroy
   has_many :categorizations, dependent: :destroy
