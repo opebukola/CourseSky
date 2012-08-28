7e7119cd58a1f92c8156aa8f2bb476844bd931ec
Ope <ope.bukola@gmail.com>
2012-08-28 12:34:05 -0400

    design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :require_beta_login
+
+  protected
+  	def require_beta_login
+  		authenticate_or_request_with_http_basic do |username, password|
+        username == "go" && password == "fish"
+      end
+  	end
 
   private
   	def admin_user
