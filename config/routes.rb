3cf4b23ab8a229d408ff788b553e650ed1ce75d8
liufengyun <liufengyunchina@gmail.com>
2012-09-27 17:17:04 +0800

    use space instead of tab for indenting

--- config/routes.rb
+++ config/routes.rb
@@ -12,33 +12,36 @@ Coursesky::Application.routes.draw do
       get :dashboard
     end
   end
+
   resources :courses do
     resources :lessons do
       collection do
         post :sort
       end
     end
+
     collection do
       get :admin
     end
-  	member do
-  		get :manage, :students
-  		put :publish, :feature
-  	end
+
+    member do
+      get :manage, :students
+      put :publish, :feature
+    end
   end
+
   resources :questions do
     member do
       post :check
     end
   end
+
   resources :question_attempts, only: [:destroy]
   resources :categories
   resources :enrollments, only: [:create, :destroy]
   resources :course_reviews
   resources :comments, only: [:new, :create, :destroy]
   resources :grade_levels
-
-
 
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact' 
